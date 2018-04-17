359c4b6fc9489b8b9126b261ebcbdf3a68b99d7c
System Administrator <root@VENKATAs-iMac.local>
2018-04-16 22:58:45 -0500

    Test controller

--- src/main/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/controllers/EstimateTimeController.java
+++ src/main/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/controllers/EstimateTimeController.java
@@ -1,4 +1,13 @@
 package com.aa.hackwars5.hackoverflow.SpringBootMiddleware.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EstimateTimeController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello";
+    }
 }
