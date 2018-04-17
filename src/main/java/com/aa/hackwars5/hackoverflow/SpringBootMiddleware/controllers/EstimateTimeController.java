944197309dfa285039f8e8129a1e8c3a0a727424
System Administrator <root@VENKATAs-iMac.local>
2018-04-17 00:09:53 -0500

    mocked estimate service

--- src/main/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/controllers/EstimateTimeController.java
+++ src/main/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/controllers/EstimateTimeController.java
@@ -1,6 +1,7 @@
 package com.aa.hackwars5.hackoverflow.SpringBootMiddleware.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,20 @@ public class EstimateTimeController {
     @GetMapping("/test")
     public String test(){
         return "Hello";
+    }
+
+    @GetMapping("/estimate")
+    public String estimate (
+            @RequestParam String flightNumber,
+            @RequestParam String uber,
+            @RequestParam String checkInBags,
+            @RequestParam String boardingPass
+    ) {
+        System.out.println(flightNumber);
+        System.out.println(uber);
+        System.out.println(checkInBags);
+        System.out.println(boardingPass);
+
+        return "7:10PM";
     }
 }
