793ecd3fcf1a4a8ccbbaeeb4d9d428b2b10edd69
System Administrator <root@VENKATAs-iMac.local>
2018-04-16 22:45:38 -0500

    Initial commit

---
+++ src/main/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/SpringBootMiddlewareApplication.java
@@ -0,0 +1,12 @@
+package com.aa.hackwars5.hackoverflow.SpringBootMiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMiddlewareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aa/hackwars5/hackoverflow/SpringBootMiddleware/SpringBootMiddlewareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aa.hackwars5.hackoverflow.SpringBootMiddleware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMiddlewareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
